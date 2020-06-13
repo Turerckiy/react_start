044bee85433043ee109fab3cf2d2924d7e6de4ab
Tureckiy <ev.zhdanov@gmail.com>
2020-06-13 23:38:53 +0300

    clean raect

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Examples} from "./Examples";
+
+export const App = () => (
+  <>
+      <Examples/>
+  </>
+);
---
+++ src/components/Examples.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Examples = ({ id, title, sku }) => {
+  return <>
+  </>;
+};
